c5947d4f72be1773459d8f778d975bf4ea6efcc8
Your Name <you@example.com>
2021-03-29 16:29:29 -0300

    formularios de edicion terminados

--- resources/views/emprendimiento/create.blade.php
+++ resources/views/emprendimiento/create.blade.php
@@ -19,111 +19,124 @@
  crossorigin=""></script>
  <link href= "{{ asset('css\create-edit.css') }}" rel="stylesheet" />
    </head>
+
+
+
 <section>
+  <div class="container">
 
-  <p class="mis-titulos">Crear Emprendimientos</p>
-   
-   {{--  @if(count($errors)>0)
+        <p class="titulo">Crear Emprendimientos</p>
+        <br>
+        <br>
+        
+        {{--  @if(count($errors)>0)
 
- <ul>   
-    @foreach ($errors->all() as $error)
+      <ul>   
+          @foreach ($errors->all() as $error)
 
-        <li> {{ $error }} </li>
-      
-    @endforeach
-    
-</ul>
+              <li> {{ $error }} </li>
+            
+          @endforeach
+          
+      </ul>
 
-@endif  --}}
-      <form id="Form" action="/emprendimiento" method="POST" enctype="multipart/form-data" class = "needs-validation" novalidate>
-    @csrf
+      @endif  --}}
+            <form id="Form" action="/emprendimiento" method="POST" enctype="multipart/form-data" class = "needs-validation" novalidate>
+          @csrf
+      <div class="container conteiner">
+        <div class="mb-3">
+          <label for="" class="form-label">Nombre</label>
+          <input id="nombre" name="nombre" type="text" class="form-control input " required>
+          <div class = "valid-feedback">¡Campo completado!</div>
+          <div class = "invalid-feedback">Campo obligatorio.</div>
+        </div>
+        
+        <div class="mb-3">
+          <label for="" class="form-label">Descripción</label>
+          <input id="descripcion" name="descripcion" type="text" class="form-control input" required>
+          <div class = "invalid-feedback">Campo obligatorio.</div>
+        </div>
+        <div class="mb-3">
+          <label for="" class="form-label">Localidad</label>
 
-  <div class="mb-3">
-    <label for="" class="form-label">Nombre</label>
-    <input id="nombre" name="nombre" type="text" class="form-control" required>
-    <div class = "valid-feedback">¡Campo completado!</div>
-    <div class = "invalid-feedback">Campo obligatorio.</div>
-  </div>
-  
-  <div class="mb-3">
-    <label for="" class="form-label">Descripción</label>
-    <input id="descripcion" name="descripcion" type="text" class="form-control" required>
-    <div class = "invalid-feedback">Campo obligatorio.</div>
-  </div>
-  <div class="mb-3">
-    <label for="" class="form-label">Localidad</label>
+          <select id="localidad_id" name="localidad_id"  class="form-control input" required>
+              <option value="">Seleccione una Localidad</option>
+                  @foreach($localidads as $localidad)
+                  <option value="{{$localidad['id']}}">{{$localidad['nombre']}}</option>
+              @endforeach
+          </select>
+          <div class = "invalid-feedback">Campo obligatorio.</div>
+        </div>
+        <div class="mb-3">
+          <label for="" class="form-label">Dirección</label>
+          <input id="direccion" name="direccion" type="text"  class="form-control input" required>
+          <div class = "invalid-feedback">Campo obligatorio.</div>
+        </div>
+        <div class="mb-3">
+          <label for="" class="form-label">Sitio Web</label>
+          <input id="sitio_web" name="sitio_web" type="text"  class="form-control input" required>
+          <div class = "invalid-feedback">Campo obligatorio.</div>
+        </div>
+        <div class="mb-3">
+          <label for="" class="form-label">Instagram</label>
+          <input id="instagram" name="instagram" type="text"  class="form-control input" required>
+          <div class = "invalid-feedback">Campo obligatorio.</div>
+        </div>
+        <div class="mb-3">
+          <label for="" class="form-label">Facebook</label>
+          <input id="facebook" name="facebook" type="text"  class="form-control input" required>
+          <div class = "invalid-feedback">Campo obligatorio.</div>
+        </div>
+        <div class="mb-3">
+          <label for="" class="form-label">Teléfono</label>
+          <input id="nro_telefono" name="nro_telefono" type="text"  class="form-control input" required>
+          <div class = "invalid-feedback">Campo obligatorio.</div>
+        </div>
+        <div class="mb-3">
 
-    <select id="localidad_id" name="localidad_id"  class="form-control" required>
-        <option value="">Seleccione una Localidad</option>
-            @foreach($localidads as $localidad)
-            <option value="{{$localidad['id']}}">{{$localidad['nombre']}}</option>
-        @endforeach
-    </select>
-    <div class = "invalid-feedback">Campo obligatorio.</div>
-  </div>
-  <div class="mb-3">
-    <label for="" class="form-label">Dirección</label>
-    <input id="direccion" name="direccion" type="text"  class="form-control" required>
-    <div class = "invalid-feedback">Campo obligatorio.</div>
-  </div>
-  <div class="mb-3">
-    <label for="" class="form-label">Sitio Web</label>
-    <input id="sitio_web" name="sitio_web" type="text"  class="form-control" required>
-    <div class = "invalid-feedback">Campo obligatorio.</div>
-  </div>
-  <div class="mb-3">
-    <label for="" class="form-label">Instagram</label>
-    <input id="instagram" name="instagram" type="text"  class="form-control" required>
-    <div class = "invalid-feedback">Campo obligatorio.</div>
-  </div>
-  <div class="mb-3">
-    <label for="" class="form-label">Facebook</label>
-    <input id="facebook" name="facebook" type="text"  class="form-control" required>
-    <div class = "invalid-feedback">Campo obligatorio.</div>
-  </div>
-  <div class="mb-3">
-    <label for="" class="form-label">Teléfono</label>
-    <input id="nro_telefono" name="nro_telefono" type="text"  class="form-control" required>
-    <div class = "invalid-feedback">Campo obligatorio.</div>
-  </div>
-  <div class="mb-3">
+          <label for="logo" class="form-label">Logo</label>
+          <input  id="logo" class="form-control-file input" tabindex="5"  type="file"
+            {{--   class="logo"  --}}
+            name="logo"
+            accept="image/png, image/jpeg, image/gif">
+            <div class = "invalid-feedback">Campo obligatorio.</div>
+        </div>
 
-    <label for="logo" class="form-label">Logo</label>
-    <input  id="logo" class="form-control-file" tabindex="5"  type="file"
-      {{--   class="logo"  --}}
-       name="logo"
-       accept="image/png, image/jpeg, image/gif">
-       <div class = "invalid-feedback">Campo obligatorio.</div>
-  </div>
-
-  <div class="mb-3">
-  <label for="" class="form-label">Tipo de Empresa</label>
-  <select id="tipoempresa_id" name="tipoempresa_id"  class="form-control" required>
-      <option value="">Seleccione un tipo de Empresa</option>
-          @foreach($tipoempresas as $tipoempresa)
-                    <option value="{{$tipoempresa['id']}}">{{$tipoempresa['nombre']}}</option>
-      @endforeach
-      
-  </select>
-  <div class="mb-3">
-    <label for="" class="form-label">Ubicación Geográfica</label>
-    <div id="map" style="height:350px;"></div>
-    <div class = "invalid-feedback">Campo obligatorio.</div>
-  </div>
-  <div class="mb-3">
-    <div class="mb-3">
-      <label for="" class="form-label">Latitud</label>
-      <input id="latitud" name="latitud" type="text"  class="form-control" required>
-      <br>
-      <label for="" class="form-label">Longitud</label>
-      <input id="longitud" name="longitud" type="text"  class="form-control" required>
+        <div class="mb-3 input">
+        <label for="" class="form-label">Tipo de Empresa</label>
+        <select id="tipoempresa_id" name="tipoempresa_id"  class="form-control" required>
+            <option value="">Seleccione un tipo de Empresa</option>
+                @foreach($tipoempresas as $tipoempresa)
+                          <option value="{{$tipoempresa['id']}}">{{$tipoempresa['nombre']}}</option>
+            @endforeach
+            
+        </select>
+        <div class="mb-3">
+          <label for="" class="form-label">Ubicación Geográfica</label>
+          <div id="map" style="height:350px;"></div>
+          <div class = "invalid-feedback">Campo obligatorio.</div>
+        </div>
+        <div class="mb-3 ubicacion">
+          <div class="mb-3">
+            <label for="" class="form-label">Latitud</label>
+            <input id="latitud" name="latitud" type="text"  class="form-control" required>
+            <br>
+            <label for="" class="form-label">Longitud</label>
+            <input id="longitud" name="longitud" type="text"  class="form-control" required>
+          </div>
+        </div>
+          <br>
+          <br>
+      <div class="offset-1 justify-content-center">
+        <a href="/emprendimiento" class="btn boton btn-outline-danger">Cancelar</a>
+        <button type="submit" class="btn boton btn-outline-success">Guardar</button>
+      </div>
+      </form>
     </div>
   </div>
 
-  <a href="/emprendimiento" class="btn btn-secondary">Cancelar</a>
-  <button type="submit" class="btn btn-primary">Guardar</button>
-</form>
 </section>
+
 
 <script type="text/javascript" src="{{asset('js/map.js')}}"></script>
 @endsection
--- resources/views/emprendimiento/edit.blade.php
+++ resources/views/emprendimiento/edit.blade.php
@@ -16,115 +16,126 @@
 </head>
 
 <section>
-<p class="mis-titulos">Editar emprendimiento</p>
-<form id="Form" action="/emprendimiento/{{$emprendimiento->id}}" method="POST" enctype="multipart/form-data" class = "needs-validation" novalidate>
-    @csrf
-    @method('PUT')
-  <div class="mb-3">
-    <label for="" class="form-label">Nombre</label>
-    <input id="nombre" name="nombre" type="text" class="form-control" value="{{$emprendimiento->nombre}}" required>
-    <div class = "valid-feedback">¡Campo completado!</div>
-    <div class = "invalid-feedback">Campo obligatorio.</div>
-  </div>
-  <div class="mb-3">
-    <label for="" class="form-label">Descripción</label>
-    <input id="descripcion" name="descripcion" type="text" class="form-control" value="{{$emprendimiento->descripcion}}" required>
-    <div class = "valid-feedback">¡Campo completado!</div>
-    <div class = "invalid-feedback">Campo obligatorio.</div>
-  </div>
-  <div class="mb-3">
-    <label for="" class="form-label">Localidad</label>
-    <select id="localidad_id" name="localidad_id"  class="form-control" value="" required>
-    <div class = "valid-feedback">¡Campo completado!</div>
-    <div class = "invalid-feedback">Campo obligatorio.</div>      
-        <option value="">Seleccione una Localidad</option>
-        {{-- <option selected="selected" value={{$emprendimiento->localidad_id}}"></option> --}}
-            @foreach($localidads as $localidad)
-              @if (old('localidad_id') == $localidad->id)
-              <option value="{{$localidad['id']}}" selected>{{$localidad['nombre']}}</option>
-          	  @else
-                <option value="{{$localidad['id']}}">{{$localidad['nombre']}}</option>
-	          @endif
+  <div class="container">
+          <p class="titulo">Editar emprendimiento</p>
+          <form id="Form" action="/emprendimiento/{{$emprendimiento->id}}" method="POST" enctype="multipart/form-data" class = "needs-validation" novalidate>
+              @csrf
+              @method('PUT')
+          <div class="container conteiner tamaño">
+            <div class="mb-3">
+              <label for="" class="form-label">Nombre</label>
+              <input id="nombre" name="nombre" type="text" class="form-control  input " value="{{$emprendimiento->nombre}}" required>
+              <div class = "valid-feedback">¡Campo completado!</div>
+              <div class = "invalid-feedback">Campo obligatorio.</div>
+            </div>
+            <div class="mb-3">
+              <label for="" class="form-label">Descripción</label>
+              <input id="descripcion" name="descripcion" type="text" class="form-control  input " value="{{$emprendimiento->descripcion}}" required>
+              <div class = "valid-feedback">¡Campo completado!</div>
+              <div class = "invalid-feedback">Campo obligatorio.</div>
+            </div>
+            <div class="mb-3">
+              <label for="" class="form-label">Localidad</label>
+              <select id="localidad_id" name="localidad_id"  class="form-control  input " value="" required>
+              <div class = "valid-feedback">¡Campo completado!</div>
+              <div class = "invalid-feedback">Campo obligatorio.</div>      
+                  <option value="">Seleccione una Localidad</option>
+                  {{-- <option selected="selected" value={{$emprendimiento->localidad_id}}"></option> --}}
+                      @foreach($localidads as $localidad)
+                        @if (old('localidad_id') == $localidad->id)
+                        <option value="{{$localidad['id']}}" selected>{{$localidad['nombre']}}</option>
+                        @else
+                          <option value="{{$localidad['id']}}">{{$localidad['nombre']}}</option>
+                      @endif
 
 
-        @endforeach
-    </select>
-  </div>
-  <div class="mb-3">
-    <label for="" class="form-label">Dirección</label>
-    <input id="direccion" name="direccion" type="text"  class="form-control" value="{{$emprendimiento->direccion}}" required>
-    <div class = "valid-feedback">¡Campo completado!</div>
-    <div class = "invalid-feedback">Campo obligatorio.</div>
-  </div>
-  <div class="mb-3">
-    <label for="" class="form-label">Sitio Web</label>
-    <input id="sitio_web" name="sitio_web" type="text"  class="form-control" value="{{$emprendimiento->sitio_web}}" required>
-    <div class = "valid-feedback">¡Campo completado!</div>
-    <div class = "invalid-feedback">Campo obligatorio.</div>
-  </div>
-  <div class="mb-3">
-    <label for="" class="form-label">Instagram</label>
-    <input id="instagram" name="instagram" type="text"  class="form-control" value="{{$emprendimiento->instagram}}" required>
-    <div class = "valid-feedback">¡Campo completado!</div>
-    <div class = "invalid-feedback">Campo obligatorio.</div>
-  </div>
-  <div class="mb-3">
-    <label for="" class="form-label">Facebook</label>
-    <input id="facebook" name="facebook" type="text"  class="form-control" value="{{$emprendimiento->facebook}}" required>
-    <div class = "valid-feedback">¡Campo completado!</div>
-    <div class = "invalid-feedback">Campo obligatorio.</div>
-  </div>
-  <div class="mb-3">
-    <label for="" class="form-label">Teléfono</label>
-    <input id="nro_telefono" name="nro_telefono" type="text"  class="form-control" value="{{$emprendimiento->nro_telefono}}" required>
-    <div class = "valid-feedback">¡Campo completado!</div>
-    <div class = "invalid-feedback">Campo obligatorio.</div>
-  </div>
-  
-    <div class="mb-3">
-    {{-- <img src="{{ "data:image/" .emprendimiento::('id')->imageType. ";base64," .base64_encode( $emprendimiento->logo ) }}"> --}}
-    <img src="{{ asset('storage').'/'. $emprendimiento->logo}}" alt="" width="200">
-    <br>
-    <label for="logo" class="form-label">Logo</label>
-    <input  id="logo" class="form-control-file" tabindex="5"  type="file"
-      {{--   class="logo"  --}}
-       name="logo"
-       accept="image/png, image/jpeg, image/gif" value="{{$emprendimiento->logo}}">
-       
-  </div>
-  <div class="mb-3">
-  <select id="tipoempresa_id" name="tipoempresa_id"  class="form-control" value="" required>
-    <div class = "valid-feedback">¡Campo completado!</div>
-    <div class = "invalid-feedback">Campo obligatorio.</div> 
-    <option value="">Seleccione un tipo de Empresa</option>
-    {{-- <option selected="selected" value={{$emprendimiento->tipoempresa_id}}"></option> --}}
-        @foreach($tipoempresas as $tipoempresa)
-        @if (old('emprendimiento') == $tipoempresa->id)
-        <option value="{{$tipoempresa['id']}}" selected>{{$tipoempresa['nombre']}}</option>
-          @else
-          <option value="{{$tipoempresa['id']}}">{{$tipoempresa['nombre']}}</option>
-        @endif
+                  @endforeach
+              </select>
+            </div>
+            <div class="mb-3">
+              <label for="" class="form-label">Dirección</label>
+              <input id="direccion" name="direccion" type="text"  class="form-control  input " value="{{$emprendimiento->direccion}}" required>
+              <div class = "valid-feedback">¡Campo completado!</div>
+              <div class = "invalid-feedback">Campo obligatorio.</div>
+            </div>
+            <div class="mb-3">
+              <label for="" class="form-label">Sitio Web</label>
+              <input id="sitio_web" name="sitio_web" type="text"  class="form-control  input " value="{{$emprendimiento->sitio_web}}" required>
+              <div class = "valid-feedback">¡Campo completado!</div>
+              <div class = "invalid-feedback">Campo obligatorio.</div>
+            </div>
+            <div class="mb-3">
+              <label for="" class="form-label">Instagram</label>
+              <input id="instagram" name="instagram" type="text"  class="form-control  input " value="{{$emprendimiento->instagram}}" required>
+              <div class = "valid-feedback">¡Campo completado!</div>
+              <div class = "invalid-feedback">Campo obligatorio.</div>
+            </div>
+            <div class="mb-3">
+              <label for="" class="form-label">Facebook</label>
+              <input id="facebook" name="facebook" type="text"  class="form-control  input " value="{{$emprendimiento->facebook}}" required>
+              <div class = "valid-feedback">¡Campo completado!</div>
+              <div class = "invalid-feedback">Campo obligatorio.</div>
+            </div>
+            <div class="mb-3">
+              <label for="" class="form-label">Teléfono</label>
+              <input id="nro_telefono" name="nro_telefono" type="text"  class="form-control  input " value="{{$emprendimiento->nro_telefono}}" required>
+              <div class = "valid-feedback">¡Campo completado!</div>
+              <div class = "invalid-feedback">Campo obligatorio.</div>
+            </div>
+            
+              <div class="mb-3">
+              {{-- <img src="{{ "data:image/" .emprendimiento::('id')->imageType. ";base64," .base64_encode( $emprendimiento->logo ) }}"> --}}
+              <img src="{{ asset('storage').'/'. $emprendimiento->logo}}" alt="" width="200">
+              <br>
+              <label for="logo" class="form-label  input ">Logo</label>
+              <input  id="logo" class="form-control-file" tabindex="5"  type="file"
+                {{--   class="logo"  --}}
+                name="logo"
+                accept="image/png, image/jpeg, image/gif" value="{{$emprendimiento->logo}}">
+                
+            </div>
+            <div class="mb-3">
+            <select id="tipoempresa_id" name="tipoempresa_id"  class="form-control" value="" required>
+              <div class = "valid-feedback">¡Campo completado!</div>
+              <div class = "invalid-feedback">Campo obligatorio.</div> 
+              <option value="">Seleccione un tipo de Empresa</option>
+              {{-- <option selected="selected" value={{$emprendimiento->tipoempresa_id}}"></option> --}}
+                  @foreach($tipoempresas as $tipoempresa)
+                  @if (old('emprendimiento') == $tipoempresa->id)
+                  <option value="{{$tipoempresa['id']}}" selected>{{$tipoempresa['nombre']}}</option>
+                    @else
+                    <option value="{{$tipoempresa['id']}}">{{$tipoempresa['nombre']}}</option>
+                  @endif
 
-    @endforeach
-</select>
-<div class="mb-3">
-    <label for="" class="form-label">Ubicación Geográfica</label>
-    <div id="map" style="height: 350px;"></div>
-    <div class="mb-3">
-      <div class="mb-3">
-      <label for="" class="form-label">Latitud</label>
-      <input id="latitud" name="latitud" type="text" value="{{$emprendimiento->latitud}}" class="form-control" required>
-      <div class = "valid-feedback">¡Campo completado!</div>
-      <div class = "invalid-feedback">Campo obligatorio.</div>
-      <br>
-      <label for="" class="form-label">Longitud</label>
-      <input id="longitud" name="longitud" type="text" value="{{$emprendimiento->longitud}}" class="form-control" required>
-      <div class = "valid-feedback">¡Campo completado!</div>
-      <div class = "invalid-feedback">Campo obligatorio.</div>
-    </div>
-  <a href="/emprendimiento" class="btn btn-secondary">Cancelar</a>
-  <button type="submit" class="btn btn-primary">Guardar</button>
-</form>
+              @endforeach
+            </select>
+            <div class="mb-3 ">
+            <br>
+                <label for="" class="form-label">Ubicación Geográfica</label>
+                <div id="map" style="height: 350px;"></div>
+                <div class="mb-3 ">
+                  <div class="mb-3 ubicacion">
+                      <label for="" class="form-label">Latitud</label>
+                      <input id="latitud" name="latitud" type="text" value="{{$emprendimiento->latitud}}" class="form-control" required>
+                      <div class = "valid-feedback">¡Campo completado!</div>
+                      <div class = "invalid-feedback">Campo obligatorio.</div>
+                      <br>
+                      <label for="" class="form-label">Longitud</label>
+                      <input id="longitud" name="longitud" type="text" value="{{$emprendimiento->longitud}}" class="form-control" required>
+                      <div class = "valid-feedback">¡Campo completado!</div>
+                      <div class = "invalid-feedback">Campo obligatorio.</div>
+                  </div>
+                <br>
+                <br>
+                <BR>
+                    <div class="offset-1 justify-content-center">
+                      <a href="/emprendimiento" class="btn boton btn-outline-danger">Cancelar</a>
+                      <button type="submit" class="btn boton btn-outline-success">Guardar</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+  </div>
 </section>
 <script type="text/javascript" src="{{asset('js/maped.js')}}"></script>
 
